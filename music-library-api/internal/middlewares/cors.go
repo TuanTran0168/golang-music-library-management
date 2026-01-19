027d6b141c8ad712a069f31de36f307dd8719fed
TuanTran0168 <tamthy12345@gmail.com>
2026-01-19 18:06:05 +0700

    [ADD][setup/deploy-production] config cors for vercel domain

--- music-library-api/internal/middlewares/cors.go
+++ music-library-api/internal/middlewares/cors.go
@@ -12,7 +12,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		AllowOrigins: []string{
 			"http://localhost:3000",
 			"http://172.17.144.1:3000",
-			"https://golang-music-library-management.onrender.com",
+			"https://golang-music-library-management.vercel.app",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
